bed601d0252cad65304c61e92d128b9ee5dae4f0
pabsolutions <pabsolutions09@gmail.com>
2022-03-17 18:03:37 +0530

    profile image with Cloudinary

--- src/Students/Sidebar.js
+++ src/Students/Sidebar.js
@@ -47,7 +47,7 @@ console.log(profile)
                         <div className="sidebar" id="sidebar">
                             <div className="main_header text-center">
                                 <div className="heading ">
-                                    <img src={profile.profileImage? `${server}/public/profile/${profile.profileImage}` :`images/girl_avtar.png`} alt="" className="info_img" height="145px" />
+                                    <img src={profile.profileImage? profile.profileImage :`images/girl_avtar.png`} alt="" className="info_img" height="145px" />
                                     <h4 className="company">{profile.name}</h4>
                                     <p className="company_text">Web developer</p>
                                 </div>
