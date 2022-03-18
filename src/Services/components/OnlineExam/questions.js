125d1a98f15dc93c990bf3a2d4dfc5f5bcb56e22
pabsolutions <pabsolutions09@gmail.com>
2022-03-18 17:04:36 +0530

    ui changes

--- src/Services/components/OnlineExam/questions.js
+++ src/Services/components/OnlineExam/questions.js
@@ -23,20 +23,20 @@ const questions = {
         {
             questionText: 'What is Resume?',
             answerOptions: [
-                { answerText: 'The Person who is doing a Job', isCorrect: false },
-                { answerText: 'The Person who is Hiring for a Job', isCorrect: false },
-                { answerText: 'The Person who is Looking for a Job', isCorrect: true },
-                { answerText: 'All the Above', isCorrect: false },
+                { answerText: 'a formal document that a job applicant creates to itemize their qualifications for a position', isCorrect: true },
+                { answerText: 'a formal document that a job applicant creates to Unnecessary personal information', isCorrect: false },
+                { answerText: 'a formal document that a job applicant creates to Details about your hobbies and interests', isCorrect: false },
+                { answerText: 'a formal document that a job applicant creates to Inaccuracies about your qualifications or experience', isCorrect: false },
             ],
         },
     
         {
             questionText: 'What is ShortList?',
             answerOptions: [
-                { answerText: 'The Person who is doing a Job', isCorrect: false },
-                { answerText: 'The Person who is Hiring for a Job', isCorrect: false },
-                { answerText: 'The Person who is Looking for a Job', isCorrect: true },
-                { answerText: 'All the Above', isCorrect: false },
+                { answerText: 'You have cleared the round of interviews and finally only salary negotiation is left.', isCorrect: false },
+                { answerText: 'Carefully choose as being the best or most suitableb', isCorrect: false },
+                { answerText: 'You have selected among other candidate profile based upon the type of skills that you have mentioned in the resume', isCorrect: true },
+                { answerText: 'The number of people which recieves the final call for the job are the selected ones.', isCorrect: false },
             ],
         },
     
