8bd7653ca26829512a2871b96dfaac2265aba326
pabsolutions <pabsolutions09@gmail.com>
2022-04-19 18:17:44 +0530

    ad

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -481,7 +481,7 @@ const Auth = (props) => {
                                         {!subTab && (
                                             <div id="Menu1">
                                                 <form onSubmit={handleLogin} onChange={resetError}>
-                                                    <div style={{ display: "flex", justifyContent: "center" }}>
+                                                    {/* <div style={{ display: "flex", justifyContent: "center" }}>
                                                         <GoogleLogin
                                                             clientId={clientId}
                                                             buttonText="Log In with Google"
@@ -491,7 +491,7 @@ const Auth = (props) => {
                                                             style={{ marginTop: '100px' }}
                                                             isSignedIn={true}
                                                         />
-                                                    </div>
+                                                    </div> */}
                                                     <div className="input_group ">
                                                         <input type="email" name="email" className="input" placeholder="Email Address" />
                                                         <span className='input_email'> <i class="fa fa-envelope" aria-hidden="true"></i> </span>
@@ -523,6 +523,20 @@ const Auth = (props) => {
                                                             Login With OTP{" "}
                                                         </a>
                                                     </div>
+
+                                                    <div style={{ display: "flex", justifyContent: "center" }}>
+                                                        <GoogleLogin
+                                                            clientId={clientId}
+                                                            buttonText="Log In with Google"
+                                                            onSuccess={onGoogleLoginSuccess}
+                                                            onFailure={onFailure}
+                                                            cookiePolicy={'single_host_origin'}
+                                                            style={{ marginTop: '100px' }}
+                                                            isSignedIn={true}
+                                                            className="login_google" 
+                                                        />
+                                                    </div>
+
                                                     <div className="already pt-3">
                                                         <label htmlFor="signup"
                                                             onClick={() => { switchMainTab('signup'); imgColumn2(); scrollToTop(); }}>
@@ -635,7 +649,7 @@ const Auth = (props) => {
                                                 </select>
                                             </div>
 
-                                            <div style={{ display: "flex", justifyContent: "center" }}>
+                                            {/* <div style={{ display: "flex", justifyContent: "center" }}>
                                                 <GoogleLogin
                                                     clientId={clientId}
                                                     buttonText="Sign Up with Google"
@@ -649,8 +663,8 @@ const Auth = (props) => {
                                                     clientId={clientId}
                                                     buttonText="Logout"
                                                 // onLogoutSuccess={onSuccess}
-                                                ></GoogleLogout> */}
-                                            </div>
+                                                ></GoogleLogout> *
+                                            </div> */}
 
                                             <div className="input_group">
                                                 <input
@@ -729,6 +743,27 @@ const Auth = (props) => {
                                                 className="btn"
                                                 defaultValue="Signup"
                                             />
+
+                                            
+                                                <div style={{ display: "flex", justifyContent: "center"}}>
+                                                <GoogleLogin
+                                                    clientId={clientId}
+                                                    buttonText={selectedClient === "applicant" ? "Signup as Applicant" : "Signup as Recruiter"}
+                                                    onSuccess={onGoogleSignUpSuccess}
+                                                    onFailure={onFailure}
+                                                    cookiePolicy={'single_host_origin'}
+                                                    style={{ marginTop: '100px' }}
+                                                    isSignedIn={true}
+                                                    className="SignUp_google" 
+                                                />
+                                                
+                                                {/* <GoogleLogout
+                                                    clientId={clientId}
+                                                    buttonText="Logout"
+                                                // onLogoutSuccess={onSuccess}
+                                                ></GoogleLogout> */}
+                                            </div>
+
 
                                             <div className="already pt-3">
                                                 <label htmlFor="signup"
